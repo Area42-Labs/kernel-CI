95803736aaf19494c988e7632c6ac19a316cc2c7
danielle <saalimquadri1@gmail.com>
2020-08-11 12:13:41 +0530

    h,, 2

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -6,7 +6,7 @@ cd $HOME
 echo "Cloning dependencies"
 git clone --depth=1 https://github.com/pixelexperience-devices/kernel_asus_X00P -b ten kernel
 cd kernel
-git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
+git clone --depth=1 https://github.com/sreekfreak995/Clang-11.0.3.git clang
 git clone --depth=1 https://github.com/stormbreaker-project/aarch64-linux-android-4.9 gcc
 git clone --depth=1 https://github.com/stormbreaker-project/arm-linux-androideabi-4.9 gcc32
 echo "Done"
@@ -57,6 +57,8 @@ function compile() {
     make O=out ARCH=arm64 X00P_defconfig
     make -j$(nproc --all) O=out \
                              ARCH=arm64 \
+			     CC=clang \
+			     CLANG_TRIPLE=aarch64-linux-gnu- \
 			     CROSS_COMPILE=aarch64-linux-android- \
 			     CROSS_COMPILE_ARM32=arm-linux-androideabi-
 }
