082955280b963327e01e7383cd9d859eecbc1861
danielle <saalimquadri1@gmail.com>
2020-08-19 23:35:02 +0530

    K

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -5,9 +5,9 @@
 cd $HOME
 echo -e "machine github.com\n  login $GITHUB_TOKEN" > ~/.netrc
 echo "Cloning dependencies"
-git clone --depth=1 https://github.com/stormbreaker-project/vince -b release kernel
+git clone --depth=1 https://github.com/stormbreaker-project/vince -b reb kernel
 cd kernel
-git clone --depth=1 https://github.com/stormbreaker-project/proton-clang clang
+git clone --depth=1 https://github.com/sreekfreak995/Clang-11.0.3.git clang
 git clone --depth=1 https://github.com/stormbreaker-project/aarch64-linux-android-4.9 gcc
 git clone --depth=1 https://github.com/stormbreaker-project/arm-linux-androideabi-4.9 gcc32
 echo "Done"
@@ -20,7 +20,7 @@ START=$(date +"%s")
 export CONFIG_PATH=$PWD/arch/arm64/configs/vince-perf_defconfig
 PATH="${PWD}/clang/bin:${PWD}/gcc/bin:${PWD}/gcc32/bin:${PATH}"
 export ARCH=arm64
-export KBUILD_BUILD_HOST=hetzner
+export KBUILD_BUILD_HOST=circleci
 export KBUILD_BUILD_USER="saalim"
 
 # Send info to channel
@@ -58,6 +58,8 @@ function compile() {
     make O=out ARCH=arm64 vince-perf_defconfig
     make -j$(nproc --all) O=out \
                              ARCH=arm64 \
+                             CC=clang \
+                             CLANG_TRIPLE=aarch64-linux-gnu \
 			     CROSS_COMPILE=aarch64-linux-android- \
 			     CROSS_COMPILE_ARM32=arm-linux-androideabi-
 }
